85b99e4dbf0f4d29dcd0548c4654b5c417d1fcf7
EGsagon <egsagon12@gmail.com>
2023-11-25 13:40:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var clicks = []
+
+const confettis = ['yellow', 'coral', 'magenta', 'hotpink', 'purple', 'orange', 'skyblue']
+
+deviation = arr => {
+    // Calculate the standard deviation in an array
+    
+    let mean = arr.reduce((sum, num) => sum + num, 0) / arr.length
+    let diffs = arr.map(num => Math.pow(num - mean, 2))
+    let avg = diffs.reduce((sum, diff) => sum + diff, 0) / arr.length
+    
+    return Math.sqrt(avg)
+}
+
+rand = arr => {
+    // Get a random array item
+
+    return arr[ Math.floor(Math.random() * arr.length) ]
+}
+
+$('#present').on('click touchend', () => {
+    // Register clicks
+
+    clicks.push(Date.now())
+    clicks = clicks.slice(-10)
+
+    // Tilt present
+    delta = deviation(clicks)
+    angle = (Math.random() * 6 - 3) * Math.abs(300 - delta) / 100
+    $('#present').css('transform', `rotate(${angle}deg)`)
+
+    // Check if present pops
+    if (clicks.length > 9 && delta < 500) {
+
+        $('#present').addClass('pop')
+        setTimeout(() => $('#pres').addClass('opened'), 300)
+        setTimeout(() => $('#present').css('display', 'none'), 800)   
+        setTimeout(() => $('#content').css('display', 'flex'), 500)   
+
+        $('.snow').each((_, el) => {
+            $(el).css('background-color', rand(confettis))
+        })
+    }
+})
+
+$('#pres i').on('click', () => {
+    $('html, body').animate({ scrollTop: $(`#content`).offset().top }, 500)
+})
