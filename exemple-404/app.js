e632014cecb1bc19e0b8ead01bf4dc2f88481d58
EGsagon <egsagon12@gmail.com>
2023-11-27 11:32:09 +0100

    exemples

--- exemple-404/app.js
+++ exemple-404/app.js
@@ -12,12 +12,6 @@ deviation = arr => {
     return Math.sqrt(avg)
 }
 
-rand = arr => {
-    // Get a random array item
-
-    return arr[ Math.floor(Math.random() * arr.length) ]
-}
-
 $('#present').on('click touchend', () => {
     // Register clicks
 
@@ -25,21 +19,22 @@ $('#present').on('click touchend', () => {
     clicks = clicks.slice(-10)
 
     // Tilt present
-    delta = deviation(clicks)
-    angle = (Math.random() * 6 - 3) * Math.abs(300 - delta) / 100
+    angle = Math.random() * 30 - 15
     $('#present').css('transform', `rotate(${angle}deg)`)
 
     // Check if present pops
-    if (clicks.length > 9 && delta < 500) {
+    if (clicks.length > 9 && deviation(clicks) < 500) {
 
         $('#present').addClass('pop')
         $('.discover > h3').css('opacity', 0)
+
         setTimeout(() => $('#pres').addClass('opened'), 300)
         setTimeout(() => $('#present').css('display', 'none'), 800)   
         setTimeout(() => $('#content').css('display', 'flex'), 500)   
 
         $('.snow').each((_, el) => {
-            $(el).css('background-color', rand(confettis))
+            color = confettis[Math.floor(Math.random() * confettis.length)]
+            $(el).css('background-color', color)
         })
     }
 })
